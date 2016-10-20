9bf5531ac5300e1f071501f27582da5e862c5bda
Chandrapal <bnchandrapal@hotmail.com>
2016-10-20 18:10:39 +0530

    Added the project

---
+++ src/electronics/Electronics.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package electronics;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Chandrapal <chandrapal@linux.com>
+ */
+public class Electronics {
+
+    public static void main(String[] args) throws IOException {
+        KMAP.main(args);
+    }
+    
+}
